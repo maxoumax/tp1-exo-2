790e376c649221c6821dca2fa1137c56b3ae89d5
maxoumax <44801188+maxoumax@users.noreply.github.com>
2020-07-17 09:10:06 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(col, Led)
+    Led += -1
+    col += -1
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(col, Led)
+    Led += 1
+    col += 1
+})
+let col = 0
+let Led = 0
+Led = 0
+let del = 0
+col = 0
+basic.forever(function () {
+    if (Led == 4) {
+        del += 1
+    }
+    if (del == 1) {
+        Led += -1
+        led.plot(col, Led)
+        basic.pause(1000)
+        led.unplot(col, Led)
+    } else {
+        Led += 1
+        led.plot(col, Led)
+        basic.pause(1000)
+        led.unplot(col, Led)
+    }
+    if (Led == 0) {
+        del += -1
+    }
+})
